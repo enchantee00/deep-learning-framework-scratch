271282996950edabf622047d284405c45919b1bd
enchantee00 <mariasla@pusan.ac.kr>
2025-03-07 17:32:51 +0900

    step03

--- steps/step03.py
+++ steps/step03.py
@@ -5,7 +5,7 @@ class Variable:
     def __init__(self, data):
         self.data = data
 
-
+# 입출력을 Variable로 통일해주므로 여러 함수 연속 적용 가능
 class Function:
     def __call__(self, input):
         x = input.data
@@ -36,3 +36,7 @@ a = A(x)
 b = B(a)
 y = C(b)
 print(y.data)
+
+"""
+하나의 계산이 아닌 계산 그래프로 표현 -> 각 변수에 대한 미분을 효율적으로 계산 가능
+"""
