dd6120ab2560aa7d35f25a0fb4543778f728007b
enchantee00 <mariasla@pusan.ac.kr>
2025-03-07 17:19:23 +0900

    step02

--- steps/step02.py
+++ steps/step02.py
@@ -16,7 +16,7 @@ class Function:
     def forward(self, in_data):
         raise NotImplementedError()
 
-
+# Function은 데이터를 꺼내고 다시 상자에 담는 역할 -> 모든 함수가 공통적으로 제공하는 기능만 담아둠
 class Square(Function):
     def forward(self, x):
         return x ** 2
