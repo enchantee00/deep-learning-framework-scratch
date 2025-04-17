932d77769e12530aa14d7587e977b37a630843e9
enchantee00 <mariasla@pusan.ac.kr>
2025-04-17 19:53:08 +0900

    step28

--- steps/step28.py
+++ steps/step28.py
@@ -19,16 +19,21 @@ def rosenbrock(x0, x1):
 x0 = Variable(np.array(0.0))
 x1 = Variable(np.array(2.0))
 lr = 0.001
-iters = 1000
+iters = 50000
 
 for i in range(iters):
     print(x0, x1)
 
     y = rosenbrock(x0, x1)
-
+    """
+    Variable 인스턴스 반복해서 사용 -> 미분값이 누적 -> cleargrad()로 누적된 미분값 초기화
+    """
     x0.cleargrad()
     x1.cleargrad()
     y.backward()
-
+    
+    """
+    경사하강법 -> 변수의 값을 미분값의 반대 방향으로 조금씩 움직임
+    """
     x0.data -= lr * x0.grad
     x1.data -= lr * x1.grad
